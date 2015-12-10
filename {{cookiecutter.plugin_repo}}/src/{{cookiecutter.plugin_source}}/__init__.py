a433d437b987fb3f8e483ca1df45f22725847846
Victor Mireyev <victor@opennodecloud.com>
2015-12-10 13:10:18 +0300

    Update template and docs for service implementation (NC-972)

--- {{cookiecutter.plugin_repo}}/src/{{cookiecutter.plugin_source}}/__init__.py
+++ {{cookiecutter.plugin_repo}}/src/{{cookiecutter.plugin_source}}/__init__.py
@@ -2,3 +2,4 @@ from nodeconductor import _get_version
 
 __version__ = _get_version('{{ cookiecutter.plugin_source }}')
 
+default_app_config = '{{ cookiecutter.plugin_source }}.apps.{{ cookiecutter.plugin_app_config_class }}'
--- {{cookiecutter.plugin_repo}}/src/{{cookiecutter.plugin_source}}/urls.py
+++ {{cookiecutter.plugin_repo}}/src/{{cookiecutter.plugin_source}}/urls.py
@@ -1,3 +1,8 @@
 from __future__ import unicode_literals
 
+from . import views
+
 # Url registers should be here!
+
+def register_in(router):
+    pass
